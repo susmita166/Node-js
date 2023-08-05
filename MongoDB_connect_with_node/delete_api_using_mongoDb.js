5260cc0ba3b3f291321ecbf7945fda8d9b712ead
Susmita Nayak <susmita.nayak@estpl.in>
2023-08-05 13:15:21 +0530

    update api

---
+++ MongoDB_connect_with_node/delete_api_using_mongoDb.js
@@ -0,0 +1,49 @@
+const conection_mongoDB= require("./mongodb_config");
+
+const express = require('express');
+const app = express();
+
+const createSchema = new conection_mongoDB.Schema({
+    name:{
+        type : String,
+        required :true
+    },
+    Role : String,
+    Phone_number : String,
+    Location : String,
+    date: {
+        type : Date,
+        default : Date.now
+    }
+})
+
+const createCollection = new conection_mongoDB.model("Employees", createSchema);
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//To access this we have to use  http://localhost:5000/update/Software Engineer
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      // Find the document by its ID and remove it
+      const deletedData = await createCollection.findByIdAndRemove(id);
+  
+      if (!deletedData) {
+        return res.status(404).json({ error: 'Data not found' });
+      }
+  
+      res.json(deletedData);
+    } catch (error) {
+      res.status(500).json({ error: 'Error deleting data' });
+    }
+  });
+
+app.listen(5000);
+
+
+
+
