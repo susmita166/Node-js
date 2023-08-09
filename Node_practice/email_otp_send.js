747f49cccd5d122f1f7aae7b4dd641f865239361
Susmita Nayak <susmita.nayak@estpl.in>
2023-08-09 11:14:47 +0530

    Smtp email configuration Successfully

--- Node_practice/email_otp_send.js
+++ Node_practice/email_otp_send.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
@@ -9,35 +9,34 @@ const transporter = nodemailer.createTransport({
       pass: 'cyweivjqdhtekdpy'
     }
 });
-//var transporter = nodemailer.createTransport(smtpConfig);
+
+//This line is for verify tranpoter configuration is success or not
 transporter.verify((error, success) => {
     if (error) {
       console.error('Transporter verification failed:', error);
     } else {
       console.log('Transporter is ready to send emails:', success);
     }
-  });
-
-// var transporter_config = nodemailer.createTransport(transporter);
-
-// Generate a random OTP
-// const otp = Math.floor(1000 + Math.random() * 9000);
-
-// console.log(otp);
-
-// const mailOptions = {
-//     from: 'acrerisefeedback@gmail.com',
-//     to: 'susmitanayak305@gmail.com', // Replace with the recipient's email address
-//     subject: 'Your OTP Code',
-//     text: `Your OTP code is: ${otp}`
-// };
+});
 
 
-// transporter_config.sendMail(mailOptions, (error, info) => {
-//     if(error){
-//         console.error('Error sending OTP email:', error);
-//     }
-//     else{
-//         console.log('OTP email sent:', info.response);
-//     }
-// })
+const otp = Math.floor(1000 + Math.random() * 9000);
+
+console.log(otp);
+
+const mailOptions = {
+    from: 'nsusmita166@gmail.com',
+    to: 'susmitanayak305@gmail.com', // Replace with the recipient's email address
+    subject: 'Your OTP Code',
+    text: `Your OTP code is: ${otp}`
+};
+
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if(error){
+        console.error('Error sending OTP email:', error);
+    }
+    else{
+        console.log('Otp send Successfully');
+    }
+})
