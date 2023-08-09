ef35ad75ac373a0f4530aab1488e2e2b33a3b61c
Susmita Nayak <susmita.nayak@estpl.in>
2023-08-09 13:06:16 +0530

    Otp send and data added Successfully

--- Mysql_connect_node_js/post_api_with_mysql.js
+++ Mysql_connect_node_js/post_api_with_mysql.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const con = require("./mysql_configuration");
 const app = express();
-
-// app.post("/",(req,resp)=>{
-//     const data = {block_name:"Phuljhari", district_id:3};
-//     con.query("INSERT INTO block SET ?", data, (error, result, fields)=>{
-//         if (error) throw error;
-//         else {
-//             resp.send(result);
-//         }
-//     });
-// });
-
-
-
-
-// If you want to insert data through postman ---
-
 app.use(express.json());
+
 app.post("/",(req,resp)=>{
     const data = req.body;
     console.log(data);
--- PROJECTBNTY/Api_for_Otp_send.js
+++ PROJECTBNTY/Api_for_Otp_send.js
@@ -7,12 +7,10 @@ const app = express();
 function generateOTP(length) {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let otp = '';
-  
     for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * chars.length);
       otp += chars.charAt(randomIndex);
     }
-  
     return otp;
 }
 
