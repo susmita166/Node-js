b17bf3744a6f5e71ba44a3c61177767ba8d17cd2
Susmita Nayak <susmita.nayak@estpl.in>
2023-08-12 18:19:30 +0530

    Adress details insert

--- PROJECTBNTY/insertAddressDetails.js
+++ PROJECTBNTY/insertAddressDetails.js
@@ -3,55 +3,96 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+//This is the example of promise
 // app.post('/insertAddress',(req,resp)   =>{
 //     const {email} = req.body;
 //     console.log(email);
 //     const email_id = email;
-//     isValidEmail(email_id, isValid =>{
-//         if (isValid) {
-//             console.log('Email address is valid.');
-//         } else {
-//             resp.json({message: 'Email address is not valid.Please insert proper email address'});
-//         }
+
+//     emailValidation(email_id)
+//     .then(successMessage => {
+//         resp.json({message: 'Email address is valid',successMessage});
+//     })
+//     .catch(error => {
+//         resp.status(400).json({ error: error.message });
 //     });
+    
 // });
 
 
-// function isValidEmail(getEmail, callback) 
-// {
-//     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//     callback(emailRegex.test(getEmail));
+// function emailValidation(getEmail){
+//     return new Promise((resolve, reject) =>{
+//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//         if (emailRegex.test(getEmail)) {
+//             resolve(true);
+//         } else {
+//             reject(new Error('Email address is not valid.'));
+//         }
+//     });
 // }
 
 
 
+app.post('/insertAddress',async (req,resp)   =>{
+    //const {email, adharNumber} = req.body;
+    const requestData = req.body;
+    //console.log(email);
+   // const email_id = email;
 
-app.post('/insertAddress',(req,resp)   =>{
-    const {email} = req.body;
-    console.log(email);
-    const email_id = email;
 
-    emailValidation(email_id)
-    .then(successMessage => {
-        resp.json({message: 'Email address is valid',successMessage});
-    })
-    .catch(error => {
+    try{
+        // await emailValidation(email);
+        // await adharValidation(adharNumber);
+        await validateConditions(requestData);
+    }
+    catch (error){
         resp.status(400).json({ error: error.message });
-    });
+    }
+    
     
 });
 
+async function validateConditions(data) {
+    try {
+        await emailValidation(data.email);
+    } catch (error) {
+        validationErrors.push(error.message);
+    }
 
-function emailValidation(getEmail){
-    return new Promise((resolve, reject) =>{
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        if (emailRegex.test(getEmail)) {
-            resolve(true);
-        } else {
-            reject(new Error('Email address is not valid.'));
-        }
-    });
+    try {
+        await adharValidation(data.adharNumber);
+    } catch (error) {
+        validationErrors.push(error.message);
+    }
 }
+
+
+
+
+// async function emailValidation(getEmail) {
+//     return new Promise((resolve, reject) =>{
+//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//         if (emailRegex.test(getEmail)) {
+//             resolve(true);
+//         } else {
+//             reject(new Error('Email address is not valid.'));
+//         }
+//     });
+// }
+
+
+// async function adharValidation(adharNumber) {
+//     return new Promise((resolve, reject) =>{
+//         const adharRegex = /^\d{12}$/;
+//         if (adharRegex.test(adharNumber)) {
+//             resolve(true);
+//         } else {
+//             reject(new Error('Invalid Aadhar number.'));
+//         }
+//     });
+// }
+
+
 
 
 
