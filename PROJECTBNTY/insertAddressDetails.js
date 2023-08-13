43d9541b7678bb761ebbc9734a711e2ffdb9d8d8
Prasanna Ghose <prasannaone0@gmail.com>
2023-08-13 21:43:50 +0530

    Show all validation in a single object

--- PROJECTBNTY/insertAddressDetails.js
+++ PROJECTBNTY/insertAddressDetails.js
@@ -1,133 +1,65 @@
-const sqlConfig = require("./Bntyy_sql_configuration");
+//const sqlConfig = require("./Bntyy_sql_configuration");
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-//This is the example of promise
-// app.post('/insertAddress',(req,resp)   =>{
-//     const {email} = req.body;
-//     console.log(email);
-//     const email_id = email;
-
-//     emailValidation(email_id)
-//     .then(successMessage => {
-//         resp.json({message: 'Email address is valid',successMessage});
-//     })
-//     .catch(error => {
-//         resp.status(400).json({ error: error.message });
-//     });
-    
-// });
 
 
-// function emailValidation(getEmail){
-//     return new Promise((resolve, reject) =>{
-//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//         if (emailRegex.test(getEmail)) {
-//             resolve(true);
-//         } else {
-//             reject(new Error('Email address is not valid.'));
-//         }
-//     });
-// }
-
-
-
-app.post('/insertAddress',async (req,resp)   =>{
-    //const {email, adharNumber} = req.body;
+app.post('/insertAddress', async (req, resp) => {
+    const { email, adharNumber } = req.body;
     const requestData = req.body;
-    //console.log(email);
-   // const email_id = email;
 
-
-    try{
-        // await emailValidation(email);
-        // await adharValidation(adharNumber);
+    try {
         await validateConditions(requestData);
-    }
-    catch (error){
-        resp.status(400).json({ error: error.message });
-    }
+    } catch (error) {
+        const dataShow = {};
+        error.forEach((err) => {
+            dataShow[err.type] = err.message;
+        });
     
-    
+        resp.status(400).json({ error: dataShow });
+      }
 });
 
-async function validateConditions(data) {
-    try {
-        await emailValidation(data.email);
-    } catch (error) {
-        validationErrors.push(error.message);
-    }
-
-    try {
-        await adharValidation(data.adharNumber);
-    } catch (error) {
-        validationErrors.push(error.message);
-    }
+async function emailValidation(getEmail) {
+    return new Promise((resolve, reject) => {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (emailRegex.test(getEmail)) {
+            resolve(true);
+        } else {
+            throw new Error('Invalid Email address');
+        }
+    });
 }
 
+async function adharValidation(adharNumber) {
+    return new Promise((resolve, reject) => {
+        const adharRegex = /^\d{12}$/;
+        if (adharRegex.test(adharNumber)) {
+            resolve(true);
+        } else {
+            throw new Error('Invalid Aadhar number');
+        }
+    });
+}
 
+async function validateConditions(data) {
+    const results = await Promise.allSettled([
+        emailValidation(data.email).catch(error => {
+            throw { type: 'Email', message: error.message };
+        }),
+        adharValidation(data.adharNumber).catch(error => {
+            throw { type: 'Adhar', message: error.message };
+        })
+    ]);
+    const errors = results
+    .filter(result => result.status === 'rejected')
+    .map(result => result.reason);
 
-
-// async function emailValidation(getEmail) {
-//     return new Promise((resolve, reject) =>{
-//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//         if (emailRegex.test(getEmail)) {
-//             resolve(true);
-//         } else {
-//             reject(new Error('Email address is not valid.'));
-//         }
-//     });
-// }
-
-
-// async function adharValidation(adharNumber) {
-//     return new Promise((resolve, reject) =>{
-//         const adharRegex = /^\d{12}$/;
-//         if (adharRegex.test(adharNumber)) {
-//             resolve(true);
-//         } else {
-//             reject(new Error('Invalid Aadhar number.'));
-//         }
-//     });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (errors.length > 0) {
+        throw errors; // throw the error array directly
+    }
+  }
 
 
 app.listen(5000);
