0d38dabb49a6a44abdea7d4f3835c7f90e072ef0
Prasanna Ghose <prasannaone0@gmail.com>
2023-08-13 22:50:06 +0530

    Show all validation in a single object

--- PROJECTBNTY/insertAddressDetails.js
+++ PROJECTBNTY/insertAddressDetails.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+//here we import the validateConditions function and use this here
+const validator = require('./validation');
+
 
 
 app.post('/insertAddress', async (req, resp) => {
@@ -10,7 +13,8 @@ app.post('/insertAddress', async (req, resp) => {
     const requestData = req.body;
 
     try {
-        await validateConditions(requestData);
+        //here we import the validator function and use this here
+        await validator(requestData);
     } catch (error) {
         const dataShow = {};
         error.forEach((err) => {
@@ -20,46 +24,5 @@ app.post('/insertAddress', async (req, resp) => {
         resp.status(400).json({ error: dataShow });
       }
 });
-
-async function emailValidation(getEmail) {
-    return new Promise((resolve, reject) => {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        if (emailRegex.test(getEmail)) {
-            resolve(true);
-        } else {
-            throw new Error('Invalid Email address');
-        }
-    });
-}
-
-async function adharValidation(adharNumber) {
-    return new Promise((resolve, reject) => {
-        const adharRegex = /^\d{12}$/;
-        if (adharRegex.test(adharNumber)) {
-            resolve(true);
-        } else {
-            throw new Error('Invalid Aadhar number');
-        }
-    });
-}
-
-async function validateConditions(data) {
-    const results = await Promise.allSettled([
-        emailValidation(data.email).catch(error => {
-            throw { type: 'Email', message: error.message };
-        }),
-        adharValidation(data.adharNumber).catch(error => {
-            throw { type: 'Adhar', message: error.message };
-        })
-    ]);
-    const errors = results
-    .filter(result => result.status === 'rejected')
-    .map(result => result.reason);
-
-    if (errors.length > 0) {
-        throw errors; // throw the error array directly
-    }
-  }
-
 
 app.listen(5000);
---
+++ PROJECTBNTY/validation.js
@@ -0,0 +1,43 @@
+async function validateConditions(data) {
+    const results = await Promise.allSettled([
+        emailValidation(data.email).catch(error => {
+            throw { type: 'Email', message: error.message };
+        }),
+        adharValidation(data.adharNumber).catch(error => {
+            throw { type: 'Adhar', message: error.message };
+        })
+    ]);
+    const errors = results
+    .filter(result => result.status === 'rejected')
+    .map(result => result.reason);
+
+    if (errors.length > 0) {
+        throw errors; // throw the error array directly
+    }
+}
+
+
+async function emailValidation(getEmail) {
+    return new Promise((resolve, reject) => {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (emailRegex.test(getEmail)) {
+            resolve(true);
+        } else {
+            throw new Error('Invalid Email address');
+        }
+    });
+}
+
+async function adharValidation(adharNumber) {
+    return new Promise((resolve, reject) => {
+        const adharRegex = /^\d{12}$/;
+        if (adharRegex.test(adharNumber)) {
+            resolve(true);
+        } else {
+            throw new Error('Invalid Aadhar number');
+        }
+    });
+}
+
+//here we export the validation and use in insertAddressDetails.js
+module.exports = validateConditions;
