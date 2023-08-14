f8aa1d1cc18ab2c97f5f4c45fe251e2bc471a8b2
Susmita Nayak <susmita.nayak@estpl.in>
2023-08-14 09:46:33 +0530

    validation check

--- PROJECTBNTY/insertAddressDetails.js
+++ PROJECTBNTY/insertAddressDetails.js
@@ -1,4 +1,4 @@
-//const sqlConfig = require("./Bntyy_sql_configuration");
+const sqlConfig = require("./Bntyy_sql_configuration");
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -26,3 +26,37 @@ app.post('/insertAddress', async (req, resp) => {
 });
 
 app.listen(5000);
+
+
+
+
+
+
+
+//This is the example of promise
+// app.post('/insertAddress',(req,resp)   =>{
+//     const {email} = req.body;
+//     console.log(email);
+//     const email_id = email;
+
+//     emailValidation(email_id)
+//     .then(successMessage => {
+//         resp.json({message: 'Email address is valid',successMessage});
+//     })
+//     .catch(error => {
+//         resp.status(400).json({ error: error.message });
+//     });
+    
+// });
+
+
+// function emailValidation(getEmail){
+//     return new Promise((resolve, reject) =>{
+//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//         if (emailRegex.test(getEmail)) {
+//             resolve(true);
+//         } else {
+//             reject(new Error('Email address is not valid.'));
+//         }
+//     });
+// }
